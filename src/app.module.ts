1ccffd5bfc088f85a002d40109050e8cb1833139
BangChaeul <30cmlive@naver.com>
2024-02-06 15:26:23 +0900

    add theme module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThemeModule } from './theme/theme.module';
 
 @Module({
-  imports: [],
+  imports: [ThemeModule],
   controllers: [AppController],
   providers: [AppService],
 })
