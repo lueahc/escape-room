da7820ad4c6a009a5d089ece373767d8dd67a7f0
BangChaeul <30cmlive@naver.com>
2024-02-12 13:21:19 +0900

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ThemeModule } from './theme/theme.module';
 import { DatabaseModule } from './config/database.module';
 import { UserModule } from './user/user.module';
 import { ReviewModule } from './review/review.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, ThemeModule, UserModule, ReviewModule],
+  imports: [DatabaseModule, ThemeModule, UserModule, ReviewModule, AuthModule],
   controllers: [],
   providers: [],
 })
