98ca28a61e0fe4cc282e8ce3253e60e743246f2b
lueahc <30cmlive@naver.com>
2024-03-03 13:26:45 +0900

    feat: 중복 review 확인 로직 (#61)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import 'dotenv/config';
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: {
-        expiresIn: '60m'
+        expiresIn: process.env.JWT_EXPIRES_IN
       },
     }),
   ],
