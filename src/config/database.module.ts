b04a67e016872ce28b4b24193cbc6f3f520e338e
BangChaeul <30cmlive@naver.com>
2024-03-05 13:49:02 +0900

    feat: 환경변수 분리

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -11,6 +11,7 @@ import 'dotenv/config';
             useFactory: () => {
                 return {
                     type: process.env.DB_TYPE as any,
+                    host: process.env.DB_HOST,
                     port: process.env.DB_PORT,
                     username: process.env.DB_USERNAME,
                     password: process.env.DB_PASSWORD,
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({
+  path: path.resolve(
+    process.env.NODE_ENV === 'production'
+      ? '.production.env'
+      : process.env.NODE_ENV === 'stage'
+        ? '.stage.env'
+        : '.development.env',
+  ),
+});
 
 async function bootstrap() {
   initializeTransactionalContext();
