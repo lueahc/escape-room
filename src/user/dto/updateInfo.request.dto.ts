a8826ab0b16ed027e09c420b9e4237c143adaf59
BangChaeul <30cmlive@naver.com>
2024-03-20 14:22:20 +0900

    feat: user swagger 추가

--- src/user/dto/updateInfo.request.dto.ts
+++ src/user/dto/updateInfo.request.dto.ts
@@ -1,7 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class UpdateInfoRequestDto {
+    @ApiProperty({ description: '비밀번호' })
     password: string;
 
+    @ApiProperty({ description: '닉네임' })
     nickname: string;
 }
