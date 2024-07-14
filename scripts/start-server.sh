96031e4df82e692fc98ff5482e6773e6d220a6ba
BangChaeul <30cmlive@naver.com>
2024-07-14 17:45:50 +0900

    빌드파일 테스트 13

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/instagram-server
+cd /home/ubuntu/escape-room-dev
 npm i
 pm2 kill
 pm2 start ecosystem.config.js --env development --name "dev-backend-server"
