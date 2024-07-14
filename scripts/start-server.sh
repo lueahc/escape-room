1e8ba8846971b3b2d97be2827837a125630aa23d
BangChaeul <30cmlive@naver.com>
2024-07-14 14:31:26 +0900

    빌드파일 테스트 11

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -e  # 오류 발생 시 스크립트 종료
+
 echo "--------------- 서버 배포 시작 -----------------"
 
 BRANCH=$(cat /home/ubuntu/temp-server/branch.txt)
+echo "Current branch is: $BRANCH"
 
 if [ "$BRANCH" = "main" ]; then
   DIRECTORY="escape-room"
@@ -21,8 +24,20 @@ chown -R ubuntu:ubuntu /home/ubuntu/$DIRECTORY
 chmod -R 755 /home/ubuntu/$DIRECTORY
 
 cp -r /home/ubuntu/temp-server/* /home/ubuntu/$DIRECTORY/
+echo "Files copied to $DIRECTORY"
+
 rm -rf /home/ubuntu/temp-server/*
+echo "Temp server directory cleared"
+
 cd /home/ubuntu/$DIRECTORY
+echo "Changed directory to $DIRECTORY"
+
 npm i
+echo "npm dependencies installed"
+
 pm2 stop $APP_NAME || true
+echo "pm2 application stopped (if it was running)"
+
 pm2 start ecosystem.config.js --env $APP_STATUS
+echo "start pm2 application"
+pm2 list
