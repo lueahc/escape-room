c438f789a99d4fa792e458d41133647af5ca0012
lueahc <lueahc@gmail.com>
2024-07-23 19:44:29 +0900

    fix

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # 스크립트가 실패할 경우 전체 배포 실패
-set -e
 echo "--------------- 서버 배포 시작 -----------------"
 cd /home/ubuntu/escape-room
 docker-compose down
 docker-compose pull
-docker-compose up -d
+docker-compose up --build -d
 echo "--------------- 서버 배포 끝 ------------------"
