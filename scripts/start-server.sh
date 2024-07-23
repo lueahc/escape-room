dbf962fa123e84fcfabaa2f3b16a6235f2e51ccc
lueahc <lueahc@gmail.com>
2024-07-23 12:24:56 +0900

    수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+# #!/bin/bash
 
-echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/escape-room-dev
-npm i
-pm2 kill
-pm2 start ecosystem.config.js --env development --name "dev-backend-server"
-echo "--------------- 서버 배포 끝 -----------------"
-pm2 list
+# echo "--------------- 서버 배포 시작 -----------------"
+# cd /home/ubuntu/escape-room-dev
+# npm i
+# pm2 kill
+# pm2 start ecosystem.config.js --env development --name "dev-backend-server"
+# echo "--------------- 서버 배포 끝 -----------------"
+# pm2 list
