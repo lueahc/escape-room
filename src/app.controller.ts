63c4e30290380ccce7d4a577f9188d38979edadc
lueahc <lueahc@gmail.com>
2024-07-24 03:30:26 +0900

    script 수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,6 +3,10 @@ import { Response } from 'express';
 
 @Controller()
 export class AppController {
+    @Get('/')
+    home(@Res() res: Response) {
+        return res.status(HttpStatus.OK).send('lets-escape-room');
+    }
     @Get('/health')
     healthCheck(@Res() res: Response) {
         return res.status(HttpStatus.OK).send('상태 검사 성공');
