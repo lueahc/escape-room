4fd38969c2c57121a88228bb0cdfef30642e0d4f
lueahc <lueahc@gmail.com>
2024-07-23 15:54:05 +0900

    add: docker

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,9 +1,10 @@
-# #!/bin/bash
+#!/bin/bash
 
-# echo "--------------- 서버 배포 시작 -----------------"
-# cd /home/ubuntu/escape-room-dev
-# npm i
-# pm2 kill
-# pm2 start ecosystem.config.js --env development --name "dev-backend-server"
-# echo "--------------- 서버 배포 끝 -----------------"
-# pm2 list
+# 스크립트가 실패할 경우 전체 배포 실패
+set -e
+echo "--------------- 서버 배포 시작 -----------------"
+cd /home/ec2-user/app
+docker-compose down
+docker-compose pull
+docker-compose up -d
+echo "--------------- 서버 배포 끝 ------------------"
