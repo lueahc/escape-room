0ecb652db3cd59f864d6cf540d4f5895bfcf61a8
lueahc <lueahc@gmail.com>
2024-09-01 13:57:32 +0900

    refactor: user 테스트 코드

--- src/user/test/user.e2e.spec.ts
+++ src/user/test/user.e2e.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from "@nestjs/common";
-import { AppModule } from "../../app.module";
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '../../app.module';
 import * as request from 'supertest';
 import { DataSource } from 'typeorm';
 import { initializeTransactionalContext } from 'typeorm-transactional';
@@ -8,270 +8,174 @@ import { UserRepository } from '../domain/user.repository';
 import { USER_REPOSITORY } from '../../common/inject.constant';
 
 describe('UserController (E2E)', () => {
-    let app: INestApplication;
-    let dataSource: DataSource;
-    let userRepository: UserRepository;
+  let app: INestApplication;
+  let dataSource: DataSource;
+  let userRepository: UserRepository;
 
-    beforeAll(async () => {
-        initializeTransactionalContext();
+  beforeAll(async () => {
+    initializeTransactionalContext();
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    app = moduleRef.createNestApplication();
+    await app.init();
 
-        const moduleRef: TestingModule = await Test.createTestingModule({
-            imports: [AppModule],
-        }).compile();
+    dataSource = moduleRef.get<DataSource>(DataSource);
+    userRepository = moduleRef.get<UserRepository>(USER_REPOSITORY);
+  });
 
-        app = moduleRef.createNestApplication();
-        await app.init();
+  beforeEach(async () => {
+    await dataSource.dropDatabase();
+    await dataSource.synchronize();
+  });
 
-        dataSource = moduleRef.get<DataSource>(DataSource);
-        userRepository = moduleRef.get<UserRepository>(USER_REPOSITORY);
+  const signUpUser = async (
+    email: string,
+    password: string,
+    nickname: string,
+  ) => {
+    return await request(app.getHttpServer()).post('/user/signUp').send({
+      email,
+      password,
+      nickname,
+    });
+  };
+
+  const signInUser = async (email: string, password: string) => {
+    return await request(app.getHttpServer()).post('/user/signIn').send({
+      email,
+      password,
+    });
+  };
+
+  const getAccessToken = async (
+    email: string,
+    password: string,
+  ): Promise<string> => {
+    const response = await signInUser(email, password);
+    return response.body.accessToken;
+  };
+
+  describe('POST /user/signUp', () => {
+    it('사용자를 생성한다.', async () => {
+      // given: 테스트 전 필요한 데이터와 환경 세팅
+
+      // when: 실행
+      const response = await signUpUser('test@test.com', 'test1234', 'tester');
+
+      // then: 결과와 기대값 비교
+      expect(response.status).toBe(201);
+      const user = await userRepository.findOneByEmail('test@test.com');
+      expect(user).toBeDefined();
     });
 
-    beforeEach(async () => {
-        await dataSource.dropDatabase();
-        await dataSource.synchronize();
+    it('이미 존재하는 이메일이면 409 에러가 발생한다.', async () => {
+      await signUpUser('test@test.com', 'test1234', 'tester1');
+      const response = await signUpUser('test@test.com', 'test1234', 'tester2');
+      expect(response.status).toBe(409);
     });
 
-    describe('POST /user/signUp', () => {
-        it('사용자를 생성한다.', async () => {
-            // given: 테스트 전 필요한 데이터와 환경 세팅
+    it('이미 존재하는 닉네임이면 409 에러가 발생한다.', async () => {
+      await signUpUser('test1@test.com', 'test1234', 'tester');
+      const response = await signUpUser('test2@test.com', 'test1234', 'tester');
+      expect(response.status).toBe(409);
+    });
+  });
 
-            // when: 실행
-            const response = await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test@test.com',
-                    password: 'test1234',
-                    nickname: 'tester'
-                });
-
-            // then: 결과와 기대값 비교
-            expect(response.status).toBe(201);
-            const user = await userRepository.findOneByEmail('test@test.com');
-            expect(user).toBeDefined();
-        });
-
-        it('이미 존재하는 이메일이면 409 에러가 발생한다.', async () => {
-            // given
-            await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test@test.com',
-                    password: 'test1234',
-                    nickname: 'tester1'
-                });
-
-            // when
-            const response = await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test@test.com',
-                    password: 'test1234',
-                    nickname: 'tester2'
-                });
-
-            // then
-            expect(response.status).toBe(409);
-        });
-
-        it('이미 존재하는 닉네임이면 409 에러가 발생한다.', async () => {
-            // given
-            await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test1234',
-                    nickname: 'tester'
-                });
-
-            // when
-            const response = await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test2@test.com',
-                    password: 'test1234',
-                    nickname: 'tester'
-                });
-
-            // then
-            expect(response.status).toBe(409);
-        });
+  describe('POST /user/signIn', () => {
+    it('로그인에 성공한다.', async () => {
+      await signUpUser('test1@test.com', 'test1234', 'tester1');
+      const response = await signInUser('test1@test.com', 'test1234');
+      expect(response.status).toBe(200);
+      expect(response.body.accessToken).toBeDefined();
     });
 
-    describe('POST /user/signIn', () => {
-        beforeEach(async () => {
-            await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test1234',
-                    nickname: 'tester1'
-                });
-        });
-
-        it('로그인에 성공한다.', async () => {
-            const response = await request(app.getHttpServer())
-                .post('/user/signIn')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test1234'
-                });
-
-            expect(response.status).toBe(200);
-            expect(response.body.accessToken).toBeDefined();
-        });
-
-        it('존재하지 않는 사용자가 요청하면 404 에러가 발생한다.', async () => {
-            const response = await request(app.getHttpServer())
-                .post('/user/signIn')
-                .send({
-                    email: 'test2@test.com',
-                    password: 'test1234'
-                });
-
-            expect(response.status).toBe(404);
-        });
-
-        it('비밀번호가 일치하지 않으면 400 에러가 발생한다.', async () => {
-            const response = await request(app.getHttpServer())
-                .post('/user/signIn')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test12345'
-                });
-
-            expect(response.status).toBe(400);
-        });
-    });
-    describe('PATCH /user/info', () => {
-        beforeEach(async () => {
-            await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test1234',
-                    nickname: 'tester1'
-                });
-        });
-
-        it('사용자의 정보를 수정한다.', async () => {
-            const loginResponse = await request(app.getHttpServer())
-                .post('/user/signIn')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test1234'
-                });
-            const accessToken = loginResponse.body.accessToken;
-
-            const response = await request(app.getHttpServer())
-                .patch('/user/info')
-                .set('Authorization', `Bearer ${accessToken}`)
-                .send({
-                    password: 'test12345',
-                    nickname: 'tester2'
-                });
-
-            const updatedResponse = await request(app.getHttpServer())
-                .post('/user/signIn')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test12345'
-                });
-
-            expect(response.status).toBe(204);
-            expect(updatedResponse.status).toBe(200);
-            const user = await userRepository.findOneByEmail('test1@test.com');
-            expect(user?.getNickname()).toBe('tester2');
-
-        });
-
-        it('이미 존재하는 닉네임이면 409 에러가 발생한다.', async () => {
-            await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test2@test.com',
-                    password: 'test1234',
-                    nickname: 'tester2'
-                });
-
-            const loginResponse = await request(app.getHttpServer())
-                .post('/user/signIn')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test1234'
-                });
-            const accessToken = loginResponse.body.accessToken;
-
-            const response = await request(app.getHttpServer())
-                .patch('/user/info')
-                .set('Authorization', `Bearer ${accessToken}`)
-                .send({
-                    nickname: 'tester2'
-                });
-
-            expect(response.status).toBe(409);
-        });
+    it('존재하지 않는 사용자가 요청하면 404 에러가 발생한다.', async () => {
+      const response = await signInUser('test2@test.com', 'test1234');
+      expect(response.status).toBe(404);
     });
 
-    describe('GET /user/search', () => {
-        let accessToken: string;
+    it('비밀번호가 일치하지 않으면 400 에러가 발생한다.', async () => {
+      await signUpUser('test1@test.com', 'test1234', 'tester1');
+      const response = await signInUser('test1@test.com', 'test12345');
+      expect(response.status).toBe(400);
+    });
+  });
 
-        beforeEach(async () => {
-            await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test1234',
-                    nickname: 'tester1'
-                });
-
-            const loginResponse = await request(app.getHttpServer())
-                .post('/user/signIn')
-                .send({
-                    email: 'test1@test.com',
-                    password: 'test1234'
-                });
-            accessToken = loginResponse.body.accessToken;
+  describe('PATCH /user/info', () => {
+    it('사용자의 정보를 수정한다.', async () => {
+      await signUpUser('test1@test.com', 'test1234', 'tester1');
+      const accessToken = await getAccessToken('test1@test.com', 'test1234');
+      const response = await request(app.getHttpServer())
+        .patch('/user/info')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({
+          password: 'test12345',
+          nickname: 'tester2',
         });
+      const updatedResponse = await signInUser('test1@test.com', 'test12345');
 
-        it('닉네임으로 사용자를 검색한다.', async () => {
-            await request(app.getHttpServer())
-                .post('/user/signUp')
-                .send({
-                    email: 'test2@test.com',
-                    password: 'test1234',
-                    nickname: 'tester2'
-                });
-
-            const response = await request(app.getHttpServer())
-                .get('/user/search')
-                .set('Authorization', `Bearer ${accessToken}`)
-                .query({ nickname: 'tester2' });
-
-            expect(response.status).toBe(200);
-            expect(response.body.userId).toBeDefined();
-            expect(response.body.userNickname).toBeDefined();
-        });
-
-        it('존재하지 않는 사용자를 검색하면 404 에러가 발생한다.', async () => {
-            const response = await request(app.getHttpServer())
-                .get('/user/search')
-                .set('Authorization', `Bearer ${accessToken}`)
-                .query({ nickname: 'x' });
-
-            expect(response.status).toBe(404);
-        });
-
-        it('본인이면 400 에러가 발생한다.', async () => {
-            const response = await request(app.getHttpServer())
-                .get('/user/search')
-                .set('Authorization', `Bearer ${accessToken}`)
-                .query({ nickname: 'tester1' });
-
-            expect(response.status).toBe(400);
-        });
+      expect(response.status).toBe(204);
+      expect(updatedResponse.status).toBe(200);
+      const user = await userRepository.findOneByEmail('test1@test.com');
+      expect(user?.getNickname()).toBe('tester2');
     });
 
-    afterAll(async () => {
-        await app.close();
-    })
-})
+    it('이미 존재하는 닉네임이면 409 에러가 발생한다.', async () => {
+      await signUpUser('test1@test.com', 'test1234', 'tester1');
+      await signUpUser('test2@test.com', 'test1234', 'tester2');
+      const accessToken = await getAccessToken('test1@test.com', 'test1234');
+      const response = await request(app.getHttpServer())
+        .patch('/user/info')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send({
+          nickname: 'tester2',
+        });
+
+      expect(response.status).toBe(409);
+    });
+  });
+
+  describe('GET /user/search', () => {
+    it('닉네임으로 사용자를 검색한다.', async () => {
+      await signUpUser('test1@test.com', 'test1234', 'tester1');
+      const accessToken = await getAccessToken('test1@test.com', 'test1234');
+      await signUpUser('test2@test.com', 'test1234', 'tester2');
+
+      const response = await request(app.getHttpServer())
+        .get('/user/search')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .query({ nickname: 'tester2' });
+
+      expect(response.status).toBe(200);
+      expect(response.body.userId).toBeDefined();
+      expect(response.body.userNickname).toBeDefined();
+    });
+
+    it('존재하지 않는 사용자를 검색하면 404 에러가 발생한다.', async () => {
+      await signUpUser('test1@test.com', 'test1234', 'tester1');
+      const accessToken = await getAccessToken('test1@test.com', 'test1234');
+      const response = await request(app.getHttpServer())
+        .get('/user/search')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .query({ nickname: 'x' });
+
+      expect(response.status).toBe(404);
+    });
+
+    it('본인이면 400 에러가 발생한다.', async () => {
+      await signUpUser('test1@test.com', 'test1234', 'tester1');
+      const accessToken = await getAccessToken('test1@test.com', 'test1234');
+      const response = await request(app.getHttpServer())
+        .get('/user/search')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .query({ nickname: 'tester1' });
+
+      expect(response.status).toBe(400);
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
--- src/user/test/user.service.spec.ts
+++ src/user/test/user.service.spec.ts
@@ -8,7 +8,6 @@ import { UserService } from '../application/user.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../app.module';
 import { USER_REPOSITORY } from '../../common/inject.constant';
-import { TestUserRepository } from '../infrastructure/testUser.repository';
 import { SignUpRequestDto } from '../application/dto/signUp.request.dto';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { SignInRequestDto } from '../application/dto/signIn.request.dto';
@@ -22,32 +21,39 @@ describe('UserService', () => {
   let app: INestApplication;
   let dataSource: DataSource;
   let userService: UserService;
-  //let userRepository: TestUserRepository;
   let userRepository: UserRepository;
 
   beforeAll(async () => {
     initializeTransactionalContext();
-
     const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      // .overrideProvider(USER_REPOSITORY)
-      // .useClass(TestUserRepository)
-      .compile();
+    }).compile();
+    app = moduleRef.createNestApplication();
+    await app.init();
 
     dataSource = moduleRef.get<DataSource>(DataSource);
     userService = moduleRef.get<UserService>(UserService);
-    //userRepository = moduleRef.get<TestUserRepository>(USER_REPOSITORY);
     userRepository = moduleRef.get<UserRepository>(USER_REPOSITORY);
-
-    app = moduleRef.createNestApplication();
-    await app.init();
   });
 
   beforeEach(async () => {
     await dataSource.dropDatabase();
     await dataSource.synchronize();
   });
+
+  const createUser = async (
+    email: string,
+    password: string,
+    nickname: string,
+  ): Promise<User> => {
+    const user = await User.create({
+      email,
+      password,
+      nickname,
+    });
+    await userRepository.save(user);
+    return user;
+  };
 
   describe('signUp()', () => {
     it('이미 존재하는 이메일이 있을 경우 ConflictException 에러가 발생한다.', async () => {
@@ -56,7 +62,6 @@ describe('UserService', () => {
         password: 'test1234',
         nickname: 'tester',
       };
-
       jest
         .spyOn(userRepository, 'findOneByEmail')
         .mockResolvedValue(await User.create(signUpRequestDto));
@@ -68,11 +73,10 @@ describe('UserService', () => {
 
     it('이미 존재하는 닉네임이 있을 경우 ConflictException 에러가 발생한다.', async () => {
       const signUpRequestDto: SignUpRequestDto = {
-        email: 'test2@test.com',
+        email: 'test@test.com',
         password: 'test1234',
         nickname: 'tester',
       };
-
       jest.spyOn(userRepository, 'findOneByEmail').mockResolvedValue(null);
       jest
         .spyOn(userRepository, 'findOneByNickname')
@@ -85,23 +89,21 @@ describe('UserService', () => {
 
     it('회원가입에 성공할 경우 사용자 정보를 반환한다.', async () => {
       const signUpRequestDto: SignUpRequestDto = {
-        email: 'test3@test.com',
+        email: 'test@test.com',
         password: 'test1234',
         nickname: 'tester3',
       };
-
       jest.spyOn(userRepository, 'findOneByEmail').mockResolvedValue(null);
       jest.spyOn(userRepository, 'findOneByNickname').mockResolvedValue(null);
       jest
         .spyOn(userService, 'hashPassword')
         .mockResolvedValue('hashedPassword');
 
-      const user = await User.create({
-        email: signUpRequestDto.email,
-        password: 'hashedPassword',
-        nickname: signUpRequestDto.nickname,
-      });
-
+      const user = await createUser(
+        signUpRequestDto.email,
+        'hashedPassword',
+        signUpRequestDto.nickname,
+      );
       jest.spyOn(userRepository, 'save').mockResolvedValue(user);
       jest.spyOn(user, 'getId').mockReturnValue(1);
       jest.spyOn(user, 'getEmail').mockReturnValue(signUpRequestDto.email);
@@ -109,7 +111,6 @@ describe('UserService', () => {
         .spyOn(user, 'getNickname')
         .mockReturnValue(signUpRequestDto.nickname);
       jest.spyOn(user, 'getCreatedAt').mockReturnValue(new Date());
-
       const result = await userService.signUp(signUpRequestDto);
 
       expect(result).toEqual({
@@ -123,11 +124,11 @@ describe('UserService', () => {
 
   describe('signIn()', () => {
     it('사용자가 존재하지 않을 경우 NotFoundException 에러가 발생한다.', async () => {
-      jest.spyOn(userRepository, 'findOneByEmail').mockResolvedValue(null);
       const signInRequestDto: SignInRequestDto = {
         email: 'test@test.com',
         password: 'test1234',
       };
+      jest.spyOn(userRepository, 'findOneByEmail').mockResolvedValue(null);
 
       await expect(userService.signIn(signInRequestDto)).rejects.toThrow(
         NotFoundException,
@@ -135,17 +136,12 @@ describe('UserService', () => {
     });
 
     it('비밀번호가 일치하지 않을 경우 BadRequestException 에러가 발생한다.', async () => {
-      const user = await User.create({
-        email: 'test@test.com',
-        password: 'test1234',
-        nickname: 'tester',
-      });
-      await userRepository.save(user);
+      const user = await createUser('test@test.com', 'test1234', 'tester');
       user.getPassword = jest.fn().mockReturnValue('hashedPassword');
       jest.spyOn(userRepository, 'findOneByEmail').mockResolvedValue(user);
       jest.spyOn(bcrypt, 'compare').mockImplementation(async () => false);
       const signInRequestDto: SignInRequestDto = {
-        email: 'test@example.com',
+        email: 'test@test.com',
         password: 'password123',
       };
 
@@ -155,12 +151,7 @@ describe('UserService', () => {
     });
 
     it('로그인에 성공할 경우 토큰을 반환한다.', async () => {
-      const user = await User.create({
-        email: 'test@test.com',
-        password: 'test1234',
-        nickname: 'tester',
-      });
-      await userRepository.save(user);
+      const user = await createUser('test@test.com', 'test1234', 'tester');
       user.getPassword = jest.fn().mockReturnValue('hashedPassword');
       user.getId = jest.fn().mockReturnValue(1);
       jest.spyOn(userRepository, 'findOneByEmail').mockResolvedValue(user);
@@ -168,17 +159,16 @@ describe('UserService', () => {
       jest
         .spyOn(userService['authService'], 'signWithJwt')
         .mockReturnValue('accessToken');
-
       const signInRequestDto: SignInRequestDto = {
         email: 'test@test.com',
         password: 'test1234',
       };
       const result = await userService.signIn(signInRequestDto);
 
-      expect(result).toEqual({ accessToken: 'accessToken' });
       expect(userService['authService'].signWithJwt).toHaveBeenCalledWith({
         userId: 1,
       });
+      expect(result).toEqual({ accessToken: 'accessToken' });
     });
   });
 
@@ -188,7 +178,6 @@ describe('UserService', () => {
         password: 'newpassword123',
         nickname: 'newnickname',
       };
-
       jest.spyOn(userRepository, 'findOneById').mockResolvedValue(null);
 
       await expect(
@@ -197,52 +186,36 @@ describe('UserService', () => {
     });
 
     it('비밀번호를 성공적으로 업데이트한다.', async () => {
-      const user = await User.create({
-        email: 'test@test.com',
-        password: 'test1234',
-        nickname: 'tester',
-      });
-      await userRepository.save(user);
-
+      const user = await createUser('test@test.com', 'test1234', 'tester');
       jest.spyOn(userRepository, 'findOneById').mockResolvedValue(user);
       jest
         .spyOn(userService, 'hashPassword')
         .mockResolvedValue('hashedPassword');
-      jest.spyOn(user, 'updatePassword').mockImplementation();
+      jest
+        .spyOn(user, 'updatePassword')
+        .mockImplementation(function (hashedPassword) {
+          this.password = hashedPassword;
+        });
       jest.spyOn(userRepository, 'save').mockResolvedValue(user);
-
       const updateInfoRequestDto: UpdateInfoRequestDto = {
         password: 'newpassword123',
-        nickname: 'nickname',
       };
-
       await userService.updateInfo(1, updateInfoRequestDto);
 
-      expect(userService.hashPassword).toHaveBeenCalledWith('newpassword123');
-      expect(user.updatePassword).toHaveBeenCalledWith('hashedPassword');
-      expect(userRepository.save).toHaveBeenCalledWith(user);
+      expect(user.getPassword()).toBe('hashedPassword');
     });
 
     it('닉네임이 이미 존재하는 경우 ConflictException 에러가 발생한다.', async () => {
-      const existingUser = await User.create({
-        email: 'test2@test.com',
-        password: 'test1234',
-        nickname: 'existingnickname',
-      });
-      await userRepository.save(existingUser);
-
-      const user = await User.create({
-        email: 'test@test.com',
-        password: 'test1234',
-        nickname: 'tester',
-      });
-      await userRepository.save(user);
-
+      const existingUser = await createUser(
+        'existing@test.com',
+        'test1234',
+        'existingnickname',
+      );
+      const user = await createUser('test@test.com', 'test1234', 'tester');
       jest.spyOn(userRepository, 'findOneById').mockResolvedValue(user);
       jest
         .spyOn(userRepository, 'findOneByNickname')
         .mockResolvedValue(existingUser);
-
       const updateInfoRequestDto: UpdateInfoRequestDto = {
         password: 'test1234',
         nickname: 'existingnickname',
@@ -254,27 +227,21 @@ describe('UserService', () => {
     });
 
     it('닉네임을 성공적으로 업데이트한다.', async () => {
-      const user = await User.create({
-        email: 'test@test.com',
-        password: 'test1234',
-        nickname: 'tester',
-      });
-      await userRepository.save(user);
-
+      const user = await createUser('test@test.com', 'test1234', 'tester');
       jest.spyOn(userRepository, 'findOneById').mockResolvedValue(user);
       jest.spyOn(userRepository, 'findOneByNickname').mockResolvedValue(null);
-      jest.spyOn(user, 'updateNickname').mockImplementation();
+      jest
+        .spyOn(user, 'updateNickname')
+        .mockImplementation(function (newNickname) {
+          this._nickname = newNickname;
+        });
       jest.spyOn(userRepository, 'save').mockResolvedValue(user);
-
       const updateInfoRequestDto: UpdateInfoRequestDto = {
-        password: 'test1234',
         nickname: 'newnickname',
       };
-
       await userService.updateInfo(1, updateInfoRequestDto);
 
-      expect(user.updateNickname).toHaveBeenCalledWith('newnickname');
-      expect(userRepository.save).toHaveBeenCalledWith(user);
+      expect(user.getNickname()).toBe('newnickname');
     });
   });
 
